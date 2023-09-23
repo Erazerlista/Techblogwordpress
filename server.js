5f50335b459c95d84997ad9315c79c77e98cd2a6
Erazerlista <128334161+Erazerlista@users.noreply.github.com>
2023-09-22 19:13:40 -0700

    updates to code

--- server.js
+++ server.js
@@ -1,41 +1,44 @@
-//added debug feature
-const express = require('express');
-const session = require('express-session');
-const exphbs = require('express-handlebars');
-const helpers = require('./utils/helpers');
 const path = require('path');
+const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
+const helpers = require('./utils/helpers');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({
+    helpers
+});
+const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
-const debug = require('debug')('app:server'); 
 
-const app = express();
-const PORT = process.env.PORT || 3306;
 
 const sess = {
-  secret: 'Secret',
-  cookie: {},
-  resave: false,
-  saveUninitialized: true,
-  store: new SequelizeStore({
-    db: sequelize
-  })
+    secret: process.env.DB_SECRET,
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize,
+        checkExpirationInterval: 1000 * 60 * 10,
+        expiration: 1000 * 60 * 30
+    })
 };
 
-// Set up Handlebars.js engine with custom helpers
-const hbs = exphbs.create({ helpers });
+const app = express();
+const PORT = process.env.PORT || 3001;
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
 app.use(session(sess));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
 app.use(routes);
 
-sequelize.sync().then(() => {
-  app.listen(PORT, () => {
-    debug(`Server is running on port ${PORT}.`);
-  });
+sequelize.sync();
+
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`);
 });
